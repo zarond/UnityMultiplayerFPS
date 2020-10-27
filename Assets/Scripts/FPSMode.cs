ca5fffad5e9a81434cb62cdf5d2aa67a3d62458b
zarond <55189420+zarond@users.noreply.github.com>
2020-10-28 00:17:40 +0300

    доработал систему хитбоксов, начал делать ai, режим первого лица, добавил сцену
    Теперь персонаж не может подстрелить свой хитбокс, также все персонажи (включая игрока) устроены одинаково.

---
+++ Assets/Scripts/FPSMode.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSMode : MonoBehaviour
+{
+    public bool firstpersonmode = true;
+    public SkinnedMeshRenderer[] firstpersonrig;
+    public SkinnedMeshRenderer[] thirdpersonrig;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (firstpersonmode) ChangeMode(true);
+        else ChangeMode(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    // надо получше сделать
+    void ChangeMode(bool ch) { 
+        if (ch) {
+            firstpersonmode = true;
+            for (int i = 0; i < firstpersonrig.Length; ++i)
+                firstpersonrig[i].enabled = true;
+            for (int i = 0; i < thirdpersonrig.Length; ++i)
+                thirdpersonrig[i].shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+        }
+        else {
+            firstpersonmode = false;
+            for (int i = 0; i < firstpersonrig.Length; ++i)
+                firstpersonrig[i].enabled = false;
+            for (int i = 0; i < thirdpersonrig.Length; ++i)
+                thirdpersonrig[i].shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+        }
+    }
+}
---
+++ Assets/Scripts/ai.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ai : MonoBehaviour
+{
+    // здесь будет логика и управление бота
+    Vector3 target;
+    public MovementRigidBody movement;
+    public WeaponHolder weapon;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player").transform.position;
+        movement = GetComponent<MovementRigidBody>();
+        //movement.input.Set(1.0f, 1.0f);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        movement.spd = (Input.GetKey(KeyCode.LeftShift)) ? movement.RunSpeed : movement.speed;
+        target = GameObject.FindWithTag("Player").transform.position;
+        Vector3 vec = target - transform.position;
+        vec = transform.InverseTransformVector(vec);
+        //vec.y = 0f;
+        //vec = Vector3.ClampMagnitude(vec, 1.0f);
+        movement.input.Set(vec.x,vec.z);
+        Debug.Log(movement.input);
+    }
+
+    UnityEngine.AI.NavMeshPath pathFindPathtoPlayer() {
+        UnityEngine.AI.NavMeshPath path = new UnityEngine.AI.NavMeshPath();
+        UnityEngine.AI.NavMesh.CalculatePath(transform.position, target, -1, path);
+        //Debug.Log(path);
+        return path;
+    }
+
+
+}
